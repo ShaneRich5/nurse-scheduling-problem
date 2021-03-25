4d574f2c104d7d864a1196d3c06e843f84dde962
Shane <shane.richards121@gmail.com>
2021-03-25 01:10:36 -0400

    feat: ability to add the names of nurses

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState<string>('')
+  const [names, setNames] = useState<string[]>([])
+
+  const addName = () => {
+    if (text !== '') {
+      setNames([...names, text])
+      setText('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <button onClick={() => addName()}>Add</button>
+      <ul>
+        {names.map((name: string, index: number) =>
+          <li key={index}>{name}</li>)
+        }
+      </ul>
+    </>
+  )
 }
 
 export default App;
